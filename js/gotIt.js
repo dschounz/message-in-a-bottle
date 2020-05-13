4dc1c81674d9aef3a348496bf38c432104b7d028
dschounz <61173807+dschounz@users.noreply.github.com>
2020-05-12 17:20:26 -0700

    Update gotIt.js

--- js/gotIt.js
+++ js/gotIt.js
@@ -34,4 +34,21 @@ firebase.database().ref(_nodeFolder + '/' + _nodeId).set(_nodeObject);
 // (to test you can just paste it into the web console)
 
 }
-// createNode(folderName, test {text: "hello"});
+// createNode(folderName, "test" {text: "hello"});
+
+// the update method will update an existing node
+
+function updateNode(_nodeFolder, _nodeID, _updateObject) {
+firebase.database().ref(_nodeFolder + '/' + _nodeId).update(_updateObject);
+// this will update existing key:value pair(s) OR add new ones to your object
+// so your object might look like:
+// { existingKey: updatedKeyValue,
+//   newKey: newValue }
+// Where the existing key is updated and newKey is added
+}
+
+// And this removes an entire node from your folder
+
+function deleteNode(_nodeFolder, _nodeID) {
+firebase.database().ref(_nodeFolder + '/' + _nodeID).remove();
+}
